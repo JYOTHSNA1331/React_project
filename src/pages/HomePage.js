b6dfd4ed480a2f2b55692aa6302c1dfc9fab2271
JYOTHSNA1331 <60293594+JYOTHSNA1331@users.noreply.github.com>
2022-10-17 15:09:02 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -20,9 +20,7 @@ const HomePage = () => {
           <Link className="link" to="/bollywood">
             <img
               src="https://images.unsplash.com/photo-1508921340878-ba53e1f016ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt="sub-img"
-              width="1400px"
-              height="700px"></img>
+              alt="sub-img"></img>
            
           </Link>
         </div>
